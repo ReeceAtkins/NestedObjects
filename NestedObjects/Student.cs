ca2827117fc73e9f819b3cb27ae420b14296cb84
Reece Atkins <Reecepowered2@hotmail.com>
2023-07-12 20:31:39 -0700

    Created student and advisor classes

---
+++ NestedObjects/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+	/// <summary>
+	/// Represents a individual college student
+	/// </summary>
+	public class Student
+	{
+		/// <summary>
+		/// The legal first name
+		/// </summary>
+		public string Firstname { get; set; }
+
+		/// <summary>
+		/// The legal last name
+		/// </summary>
+		public string Lastname { get; set; }
+
+		/// <summary>
+		/// The assiged school email address
+		/// </summary>
+		public string SchoolEmail { get; set; }
+
+		/// <summary>
+		/// The phone number
+		/// </summary>
+		public string PhoneNumber { get; set; }
+
+		
+		public Advisor AssignedAdvisor { get; set; }
+	}
+}
