6bc20e2f5d8d219d40f766d31a2df7dbe91c6b7c
Reece Atkins <Reecepowered2@hotmail.com>
2023-07-12 20:48:53 -0700

    Added test code

--- NestedObjects/Program.cs
+++ NestedObjects/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using NestedObjects;
+
+Advisor compSciAdvisor = new()
+{
+	Email = "Annemarie@cptc.edu",
+	FullName = "Annemarie Smith",
+	OfficeLocation = "B17 Rm 150"
+};
+
+Student stu1 = new()
+{
+	Firstname = "Homer",
+	Lastname = "Simpson",
+	DateOfBirth = new DateOnly(2000, 3, 20),
+	AssignedAdvisor = compSciAdvisor,
+	PhoneNumber = "2535215512",
+	SchoolEmail = "Homer.Simpson@students.cptc.edu"
+};
+
+Console.WriteLine($"Student: {stu1.Firstname} has {stu1.AssignedAdvisor.FullName}");
